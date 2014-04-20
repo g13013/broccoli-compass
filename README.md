4d7e3242026c5d0ead67a4b4565ad035a8b6fdd6
Aboubakr GASMI <g13013@gmail.com>
2014-04-20 03:22:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# broccoli-compass
+
+Compiles compass project using [compass](https://github.com/chriseppstein/compass), so you need to have compass installed on your machine.
+
+## Installation
+
+```bash
+npm install --save-dev broccoli-compass
+```
+
+## Usage
+
+Note: The API might change in subsequent 0.x versions.
+
+```js
+var compileSass = require('broccoli-compass');
+var coompassTree = compileSass(tree, files, {
+    outputStyle: 'expanded' //include here compass options. Note: some options might be ignored as they are automatically filled
+  });
+
+```
+if `files` is omitted the entire folder will be compiled.
+
+### Options
+Please refere to the [compass configuration](http://compass-style.org/help/tutorials/configuration-reference/) for more details about the available options
+
+
+##License
+[MIT](https://github.com/g13013/broccoli-compass/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,91 @@
+var path = require('path');
+var exec = require('child_process').exec;
+var Writer = require('broccoli-writer');
+var rsvp = require('rsvp');
+
+function compile(cmdLine, options) {
+  return new rsvp.Promise(function(resolve, reject) {
+    exec(cmdLine, options, function(err, stdout, stderr) {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+function CompassCompiler(inputTree, files, options) {
+  var op;
+  if (arguments.length === 2 && files !== null && typeof files === 'object' && !(files instanceof Array)) {
+    options = files;
+    files = [];
+  }
+
+  if (!(this instanceof CompassCompiler)){
+    return new CompassCompiler(inputTree, files, options);
+  }
+  this.inputTree = inputTree;
+  this.files = [].concat(files || []);
+  merge(this.options, options);
+}
+
+CompassCompiler.prototype = Object.create(Writer.prototype);
+CompassCompiler.prototype.constructor = CompassCompiler;
+CompassCompiler.prototype.write = function (readTree, destDir) {
+  var self = this,
+      aArg = ['compass', 'compile'];
+  return readTree(this.inputTree).then(function (srcDir) {
+    var cmdLine;
+    var cmdArgs = aArg.concat(self.files);//src is project dir or specifed files
+    var cssDir = path.relative(srcDir, destDir) + '/css';
+    var options = merge({}, self.options);
+    options.cssDir = '"'+ cssDir + '"';
+    //options.sassDir = '"'+ srcDir + '"';
+    cmdArgs = cmdArgs.concat( generateArgs(options) );
+    cmdLine = cmdArgs.join(' ');
+    return compile(cmdLine, {cwd: srcDir}).then(function () {
+      return destDir;
+    }, function (err) {
+      console.log('[broccoli-compass] Error: ', err.message + 'The command-line arguments was: `' + cmdLine + '`');
+    });
+  });
+};
+
+CompassCompiler.prototype.options = {
+  relativeAssets: true,
+  sassDir: '.'
+};
+
+function merge(obj1, obj2) {
+  var key;
+  obj1 = obj1 || {};
+  obj2 = obj2 || {};
+  for (key in obj2) {
+    if (obj2.hasOwnProperty(key)) {
+      obj1[key] = obj2[key];
+    }
+  }
+  return obj1;
+}
+
+function generateArgs(options) {//generate command line options in format that compass understands
+  var op, option, value,
+      args = [];
+  for (op in options) {
+    if (options.hasOwnProperty(op)) {
+      value = options[op];
+      if (value === false || value === null) {
+        continue;
+      }
+      option = '--' + op.replace(/([A-Z])/, '-$1').toLowerCase();
+      if (value === true) {
+        value = '';
+      } else {
+        value = ' ' + value;
+      }
+      args.push(option + value);
+    }
+  }
+  return args;
+}
+module.exports = CompassCompiler;
