929441efc3c46409bd938aeb6f70a2a914068159
Aboubakr GASMI <g13013@gmail.com>
2014-10-26 09:42:11 +0100

    added tests for the cache

--- test/index.js
+++ test/index.js
@@ -54,6 +54,32 @@ describe('broccoli-compass', function() {
     }
   });
 
+  it('Chaches result', function() {
+    var tree = compassCompile(srcDir, defaultOptions);
+    var compile = tree.compile;
+    var calls = 0;
+    var builder = new broccoli.Builder(tree);
+    tree.compile = function () {
+      calls++;
+      return compile.apply(this, arguments);
+    };
+    return builder.build().then(function (r1) {
+        return builder.build().then(function (r2) {
+          if (r1.directory !== r2.directory || calls !== 1){
+            return expect().fail('Should return the same directory');
+          }
+          fs.writeFileSync(srcDir + '/' + sassDir + '/new.scss', '//comment');
+          return builder.build().then(function (r3) {
+            if (r1.directory === r3.directory || calls !== 2){
+              return expect().fail('Should detect changes and compile again');
+            }
+          });
+        });
+      }).catch(function (err) {
+        expect().fail(err.message);
+      });
+  });
+
   it('should ignore errors when ignoreErrors is set to TRUE', function() {
     var options = merge(defaultOptions, { compassCommand: 'notExistantTool' });
     var tree = compassCompile(srcDir, options);
